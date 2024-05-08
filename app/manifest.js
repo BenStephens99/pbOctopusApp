92079e227a8fc92d66eff1b851a294d21f2c6ae1
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2024-05-08 14:29:16 +0100

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -3,15 +3,20 @@ export default function manifest() {
     name: 'Energy App',
     short_name: 'Energy App',
     description: 'Energy App',
-    start_url: '/',
+    start_url: '/properties',
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/favicon-48.png',
+        sizes: '48x48',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-256.png',
+        sizes: '256x256',
+        type: 'image/png',
       },
     ],
   }
